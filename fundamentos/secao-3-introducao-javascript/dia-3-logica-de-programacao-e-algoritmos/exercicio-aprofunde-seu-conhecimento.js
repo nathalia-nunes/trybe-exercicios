d9b94e0722dd7f1cec6005a0a587be57015ca997
nathalia-nunes <nnunesm@gmail.com>
2023-01-25 19:25:58 -0300

    dois exercicios logica

---
+++ fundamentos/secao-3-introducao-javascript/dia-3-logica-de-programacao-e-algoritmos/exercicio-aprofunde-seu-conhecimento.js
@@ -0,0 +1,16 @@
+let resultado = 1;
+for (let index = 10; index >= 1; index -= 1) {
+    resultado *=  index; 
+}     
+console.log(resultado);
+
+let word = 'Gel problema no fone';
+let reverter = '';
+ for(let index = word.length -1; index >= 0; index -= 1) {
+   // para testar o que está sendo imprimindo no => console.log(index);
+    reverter += word[index];   
+ } console.log(reverter);
+ 
+
+
+
