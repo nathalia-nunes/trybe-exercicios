5772f761b6216ab353e7855be12ad0e6a4156b47
nathalia-nunes <nnunesm@gmail.com>
2023-01-25 16:14:08 -0300

    exercicio lidando com arrays

---
+++ fundamentos/secao-3-introducao-javascript/dia-2-array-loop-for/exercicio-lidando-com-arrays/scripts.js
@@ -0,0 +1,67 @@
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//let resultado = 0;
+
+//for (let index = 0; index < numbers.length; index += 1) {
+//    resultado += numbers[index];
+//}
+
+//console.log(resultado);
+//let soma = 0;
+
+//for (let index = 0; index < numbers.length; index += 1) {
+//    soma += numbers[index];
+//}
+
+//let media = soma / numbers.length;
+
+//console.log(media);
+
+//let maiorNumero = numbers[0];
+
+//for (let index = 1; index < numbers.length; index += 1) {
+//    if (numbers[index] > maiorNumero) {
+//        maiorNumero = numbers[index];
+//    }
+//}
+//console.log(maiorNumero);
+
+//if (media > 20) {
+//   console.log('Valor maior que 20');
+//} else {'Valor menor ou igual a 20'
+//}
+
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//let result = 0;
+
+//for (let index = 0; index < numbers.length; index += 1) {
+//  if (numbers[index] % 2 !== 0) {
+//    result += 1;
+//  }
+//}
+
+//if (result === 0) {
+//  console.log('nenhum valor ímpar encontrado');
+//} else {
+//  console.log(result);
+//}
+
+let numeros = [
+    1,  2,  3,  4,  5,  6,  7,  8,
+    9, 10, 11, 12, 13, 14, 15, 16,
+   17, 18, 19, 20, 21, 22, 23, 24,
+   25
+ ]
+
+//    for(let index = 1; index <= 25; index += 1) {
+//        numeros.push(index);
+//    }
+
+//    console.log(numeros);
+
+for (let index = 0; index < numeros.length; index += 1) {
+    console.log(numeros[index] / 2);
+  };
+
+
+
+
