67527cfbbe337cc15ceb44fea306705298c302b6
nathalia-nunes <nnunesm@gmail.com>
2023-01-28 18:25:53 -0300

    testando exercicios

---
+++ fundamentos/secao-3-introducao-javascript/dia-4-objetos-e-funcoes/testes.js
@@ -0,0 +1,17 @@
+// Requisito 1
+const girafa = true;
+const elefante = true;
+const macaco = false;
+
+const compareTrue = (animal1, animal2) => {
+    if (animal1 && animal2 === true) {
+        return true;
+    } else if (animal1 && animal2 === false) {
+        return false;
+    } else (animal1 === false && animal2 === true)
+        return false;
+    }
+
+console.log(compareTrue(elefante, macaco));
+
+// Requisito 2
