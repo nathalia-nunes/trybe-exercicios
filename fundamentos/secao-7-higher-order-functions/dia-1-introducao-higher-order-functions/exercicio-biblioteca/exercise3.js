609bf7265d27b0fb4c9a40695f1792cbb3a111c1
nathalia-nunes <nnunesm@gmail.com>
2023-03-06 14:00:31 -0300

    exercicio 3 biblioteca

---
+++ fundamentos/secao-7-higher-order-functions/dia-1-introducao-higher-order-functions/exercicio-biblioteca/exercise3.js
@@ -0,0 +1,22 @@
+const books = require('./books')
+
+const expectedResult = {
+  author: {
+    birthYear: 1948,
+    name: 'George R. R. Martin',
+  },
+  genre: 'Fantasia',
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  releaseYear: 1991,
+};
+
+const getNamedBook = () => {
+  // escreva seu código aqui
+
+  const book = books.find((book) =>
+    book.name.length === 26);
+  return book === undefined ? 'Livro não encontrado' : book
+}
+
+console.log(getNamedBook());
