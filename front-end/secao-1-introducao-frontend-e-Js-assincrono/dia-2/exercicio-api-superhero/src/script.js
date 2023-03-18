006f0df3c13479adda3317e77729f5750d6e2e54
nathalia-nunes <nnunesm@gmail.com>
2023-03-18 16:29:54 -0300

    css

--- front-end/secao-1-introducao-frontend-e-Js-assincrono/dia-2/exercicio-api-superhero/src/script.js
+++ front-end/secao-1-introducao-frontend-e-Js-assincrono/dia-2/exercicio-api-superhero/src/script.js
@@ -1,9 +1,18 @@
+import Swal from 'sweetalert2';
+
 const img = document.querySelector('#img');
 const nameSH = document.querySelector('#nameSH');
 const btnSort = document.querySelector('#btnSort');
 const urlSh = 'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id';
 const max = 750;
 const raffleId = () => Math.floor(Math.random() * max);
+
+const alert = () => Swal.fire({
+  icon: 'error',
+  title: 'Oops...',
+  text: 'Não foi possível encontrar o herói!',
+  footer: '<a href="">Why do I have this issue?</a>'
+});
 
 const getHero = () => {
   const resultId = raffleId();
@@ -12,11 +21,12 @@ const getHero = () => {
     .then((response) => {
       // console.log(response.name)
       nameSH.innerHTML = response.name;
-      img.src = response.images.md;
+      img.src = response.images.sm;
     })
-    .catch((error) => {
-      nameSH.innerHTML = 'Não foi possível encontrar o herói.';
-      console.warn(error.message);
+    .catch(() => {
+      // nameSH.innerHTML = 'Não foi possível encontrar o herói.';
+      // console.warn(error.message);
+      alert();
     });
 };
 
